8425a52d6afefd7c264f728b8e22188aceb15f9f
yosyp <y.shvab+git@gmail.com>
2018-03-12 21:04:53 -0400

    Fixed plot_trajectories() to color by time

--- src/plot_trajectories.m
+++ src/plot_trajectories.m
@@ -15,30 +15,23 @@
 function plot_trajectories(natoms, x, y, dlat, latx, laty, ...
                             displayflag, saveflag, savedir, fileprefix)     
     [~,steps] = size(x);                   
-    z = zeros(steps);
-    colors = 0:.1:steps;
-    tempfig = figure;
+    z = zeros(1,steps);
+    time = 1:steps;
+    tempfig = figure; hold on;
 
     % TODO: 
     %  Plot trajectories of each atom over simulation timeframe. Add an
     %  option that colorizes the path by time, with a time colorbar. 
-%    hold on;
-%    for j=1:floor(steps/20):steps
     for i=1:natoms
-        grid on;
         axis([-.5*laty*dlat 1.5*laty*dlat -.5*laty*dlat 1.5*laty*dlat]./1e-9);
         xlabel('x [nm]','FontWeight','bold','Color','black');
         ylabel('y [nm]','FontSize',18,'FontWeight','bold','Color','black');
         xt = get(gca, 'XTick'); set(gca, 'FontSize', 16);  set(gca, 'LineWidth', 2);
-        surf([x(i,:);x(i,:)],[y(i,:);y(i,:)],[z;z],[colors;colors],...
+        surface([x(i,:);x(i,:)]./1e-9,[y(i,:);y(i,:)]./1e-9,[z;z],[time;time],...
                 'facecol','no',...
                 'edgecol','interp',...
-                'linew',2);
-%         for i=1:natoms
-%             plot(x(i,j)./1e-9, y(i,j)./1e-9, 'bo', 'MarkerSize', 2, 'LineWidth', 4);
-%         end
+                'linew',4);        
     end
-%    end
    
     if displayflag == false
         close(tempfig);
